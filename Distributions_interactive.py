92b3bcd9924d7d8637a58a44f482c9035d76cc0a
Marco Alberti <42811127+AlbertiMarco@users.noreply.github.com>
2021-04-14 23:40:06 +0200

    Add files via upload

--- Distributions_interactive.py
+++ Distributions_interactive.py
@@ -1,48 +1,33 @@
 import pandas as pd
 from bokeh.plotting import figure
-from bokeh.io import output_file, show,output_notebook, push_notebook
+from bokeh.io import output_file, show,output_notebook
 from bokeh.io import curdoc
-from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource, DataRange1d, Select, CheckboxButtonGroup
+from bokeh.layouts import row,column
+from bokeh.models import ColumnDataSource, CDSView, GroupFilter, Select , MultiChoice
 from bokeh.palettes import Blues4
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Set2
 
-panel_dataset = pd.read_csv('https://www.dropbox.com/s/ufae6fm64anb459/data_interactive_distribution.csv?dl=1')
-
-str_status={1:'Low',2:'Middle-Low',3:'Upper-middle',4:'Upper'}
-panel_dataset.status.replace(to_replace=str_status,inplace=True)
-panel_dataset.sex.replace(to_replace={'V':'F'},inplace=True)
+panel_dataset = pd.read_csv('https://www.dropbox.com/s/pwmuo4d4675rvww/data_interactive_distribution.csv?dl=1')
 
 sex='F'
 year=2016
 years=[2011,2012,2013,2014,2015,2016]
 genders=['M','F']
 sstatus=['Low', 'Middle-Low','Upper-middle', 'Upper']
-year_select = Select(value='2016' , title='Year', options=[str(i) for i in years])
-sex_select = Select(value='F' , title='Sex', options=genders)
+year_select = Select(value=str(year) , title='Year', options=[str(i) for i in years])
+sex_select = Select(value=sex , title='Sex', options=genders)
 
 
-def get_dataset(src, sex, year):
-    df1 = src[(src.sex == sex) & (src.year == year)]
-    r=pd.DataFrame(data=df1.groupby(['age','status'])['health_expenditure_under_deductible'].sum()/df1.groupby(['age','status'])['number_citizens'].sum(),columns=['health_exp'])
-    df=df1.merge(r,left_on=['age','status'], right_on=['age','status'],how='left')
-    df.sort_values(['age'])
-    df=pd.DataFrame(df.groupby(['age','sex','status','year'])['health_exp'].mean().reset_index())
-    return ColumnDataSource(data=df)
-
-def make_plot(source, title):
-    plot = figure(plot_width=1000, x_axis_label='Age', y_axis_label='Health expenditure €')
-    plot.title.text = title
-    colors=['darkblue','cornflowerblue','forestgreen', 'yellowgreen']
-    df=source.to_df()
+def make_plot(sources, title):
     lines = {}
-    for status,name,color in zip(sstatus,sstatus,colors):
-        temp_source=ColumnDataSource(data=df[df['status']==status])
-        lines[name]=plot.line(x='age',y='health_exp',source=temp_source,line_width=2,line_color=color,legend_label=status)
-        lines[name].visible = True if (name == 'Low' or name=='Upper') else False
+    plot = figure(plot_width=1000, x_axis_label='Age', y_axis_label='Health expenditure €')
+    colors=['darkblue','cornflowerblue','forestgreen', 'yellowgreen']
+    for status,color,source in zip(sstatus,colors,sources):
+        lines[status]=plot.line(x='age',y='health_exp',source=source,line_width=2,line_color=color,legend_label=status)
+        lines[status].visible = True if (status == 'Low' or status=='Upper') else False
 
     # fixed attributes
     plot.legend.location = "top_left"
@@ -55,19 +40,31 @@ def make_plot(source, title):
     plot.title.text_font_style='bold'
     plot.title.align='center'
     plot.axis.axis_label_text_font_size='12pt'
+    plot.title.text = title
 
     return plot
-
-source=get_dataset(panel_dataset,sex, year)
-plot=make_plot(source,"Health expenditure per head by socio-economic status in "+str(year)+' for '+sex)
 
 def update_plot(attrname, old, new):
     year=int(year_select.value)
     sex=sex_select.value
-    src=get_dataset(panel_dataset,sex,year)
-    source.data.update(src.data)
-    title=plot.title.text="Health expenditure per head by socio-economic status in "+str(year)+' for '+sex
-    make_plot(source, title)
+    title="Health expenditure per head by socio-economic status in "+str(year)+' for '+sex
+    plot.title.text=title
+    src1 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[0])])
+    src2 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[1])])
+    src3 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[2])])
+    src4 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[3])])
+    source1.data.update(src1.data)
+    source2.data.update(src2.data)
+    source3.data.update(src3.data)
+    source4.data.update(src4.data)
+
+source1 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[0])] )
+source2 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[1])] )
+source3 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[2])] )
+source4 = ColumnDataSource(data=panel_dataset[(panel_dataset.sex == sex) & (panel_dataset.year == year) & (panel_dataset['status']==sstatus[3])] )
+sources=[source1,source2,source3,source4]
+title = "Health expenditure per head by socio-economic status in "+str(year)+' for '+sex
+plot = make_plot(sources,title)
 
 year_select.on_change('value', update_plot)
 sex_select.on_change('value',update_plot)
