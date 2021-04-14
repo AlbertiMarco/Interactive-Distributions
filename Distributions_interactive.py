401ea0255968b350a539ac02493618ab3f97fedf
Marco Alberti <42811127+AlbertiMarco@users.noreply.github.com>
2021-04-14 13:16:59 +0200

    Add files via upload

--- Distributions_interactive.py
+++ Distributions_interactive.py
@@ -10,29 +10,23 @@ from bokeh.application import Application
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Set2
 
-#Read data from dropbox
-
-panel_dataset = pd.read_csv('https://www.dropbox.com/s/jolb750wierrt0h/panel_dataset.csv?dl=1')
-
-
-# In[5]:
-
+panel_dataset = pd.read_csv('https://www.dropbox.com/s/ufae6fm64anb459/data_interactive_distribution.csv?dl=1')
 
 str_status={1:'Low',2:'Middle-Low',3:'Upper-middle',4:'Upper'}
 panel_dataset.status.replace(to_replace=str_status,inplace=True)
-
-
-# In[6]:
-
-
 panel_dataset.sex.replace(to_replace={'V':'F'},inplace=True)
 
-
-# In[7]:
+sex='F'
+year=2016
+years=[2011,2012,2013,2014,2015,2016]
+genders=['M','F']
+sstatus=['Low', 'Middle-Low','Upper-middle', 'Upper']
+year_select = Select(value='2016' , title='Year', options=[str(i) for i in years])
+sex_select = Select(value='F' , title='Sex', options=genders)
 
 
 def get_dataset(src, sex, year):
-    df1 = src[src.sex == sex][src.year == year]
+    df1 = src[(src.sex == sex) & (src.year == year)]
     r=pd.DataFrame(data=df1.groupby(['age','status'])['health_expenditure_under_deductible'].sum()/df1.groupby(['age','status'])['number_citizens'].sum(),columns=['health_exp'])
     df=df1.merge(r,left_on=['age','status'], right_on=['age','status'],how='left')
     df.sort_values(['age'])
@@ -44,9 +38,11 @@ def make_plot(source, title):
     plot.title.text = title
     colors=['darkblue','cornflowerblue','forestgreen', 'yellowgreen']
     df=source.to_df()
-    for status,color in zip(sstatus,colors):
+    lines = {}
+    for status,name,color in zip(sstatus,sstatus,colors):
         temp_source=ColumnDataSource(data=df[df['status']==status])
-        plot.line(x='age',y='health_exp',source=temp_source,line_width=2,line_color=color,legend_label=status)
+        lines[name]=plot.line(x='age',y='health_exp',source=temp_source,line_width=2,line_color=color,legend_label=status)
+        lines[name].visible = True if (name == 'Low' or name=='Upper') else False
 
     # fixed attributes
     plot.legend.location = "top_left"
@@ -62,24 +58,16 @@ def make_plot(source, title):
 
     return plot
 
-sex='F'
-year=2016
-years=[2011,2012,2013,2014,2015,2016]
-genders=['M','F']
-sstatus=['Low', 'Middle-Low','Upper-middle', 'Upper']
-
-year_select = Select(value='2011' , title='Year', options=[str(i) for i in years])
-sex_select = Select(value='F' , title='Sex', options=genders)
+source=get_dataset(panel_dataset,sex, year)
+plot=make_plot(source,"Health expenditure per head by socio-economic status in "+str(year)+' for '+sex)
 
 def update_plot(attrname, old, new):
     year=int(year_select.value)
     sex=sex_select.value
-    title=plot.title.text="Health expenditure per head by socio-economic status in "+str(year)+' for '+sex
     src=get_dataset(panel_dataset,sex,year)
     source.data.update(src.data)
-
-source=get_dataset(panel_dataset,sex, year)
-plot=make_plot(source,"Health expenditure per socio-economic status in "+str(year))
+    title=plot.title.text="Health expenditure per head by socio-economic status in "+str(year)+' for '+sex
+    make_plot(source, title)
 
 year_select.on_change('value', update_plot)
 sex_select.on_change('value',update_plot)
@@ -88,9 +76,3 @@ controls = column(year_select,sex_select)
 
 layout=(row(plot,controls))
 curdoc().add_root(layout)
-
-#Display plot inline in Jupyter notebook
-output_notebook()
-
-#Display plot
-show(layout)
